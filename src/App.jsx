a1fcdee8aefe0bbc8469fd1d23630151570984c3
unknown <m.mehdi.paikane@gmail.com>
2024-10-25 23:12:27 +0330

    do half of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import Header from "./components/Header";
+import Quiz from "./components/Qiuz";
 
 function App() {
-
   return (
     <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
