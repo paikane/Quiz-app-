a372cde4947dfd7ea9ae9e2316f6f4bd17a8113f
unknown <m.mehdi.paikane@gmail.com>
2024-10-26 10:27:48 +0330

    update progress bar

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,25 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log('SETTING TIMEOUT');
+    console.log("SETTING TIMEOUT");
     const timer = setTimeout(onTimeout, timeout);
     return () => {
-        clearTimeout(timer)
-    }
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('SETTING INTERVAL');
+    console.log("SETTING INTERVAL");
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
     return () => {
-        clearInterval(interval)
-    }
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
